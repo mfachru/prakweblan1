e7c9e87f7ffde8ffa74d7c083bd148923479bd30
Muhammad Fachru Syahputra <fachrusyahputra@gmail.com>
2022-09-20 19:13:16 +0700

    praktikum

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -9,6 +9,9 @@ class Mahasiswa extends Model
     protected $table            = 'mahasiswa';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
+    protected $allowedFields    = ['npm', 'nama', 'alamat'];
+
+    // Dates
     protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
